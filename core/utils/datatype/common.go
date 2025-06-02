6df5609fbc767916b35d921d6b1083fd2b7135c0
guyi <guyigood@163.com>
2025-06-02 08:49:42 +0800

    在 go.mod 和 go.sum 文件中添加了 tjfoc/gmsm 依赖，并在 common.go 文件中新增多个类型转换函数，包括从 interface 转换为 float64、bool、[]byte、map[string]interface{} 及从 string 转换为 int、float64、bool、[]byte、map[string]interface{} 和 []interface{}，以增强数据处理的灵活性和可用性。

--- core/utils/datatype/common.go
+++ core/utils/datatype/common.go
@@ -1,6 +1,7 @@
 package datatype
 
 import (
+	"encoding/json"
 	"fmt"
 	"strconv"
 )
@@ -14,4 +15,62 @@ func TypetoStr(data interface{}) string {
 func TypetoInt(data interface{}) (int, error) {
 	result := fmt.Sprintf("%v", data)
 	return strconv.Atoi(result)
+}
+
+// interface转float64
+func TypetoFloat64(data interface{}) (float64, error) {
+	result := fmt.Sprintf("%v", data)
+	return strconv.ParseFloat(result, 64)
+}
+
+// interface转bool
+func TypetoBool(data interface{}) (bool, error) {
+	result := fmt.Sprintf("%v", data)
+	return strconv.ParseBool(result)
+}
+
+// interface转[]byte
+func TypetoBytes(data interface{}) ([]byte, error) {
+	return json.Marshal(data)
+}
+
+// interface转map[string]interface{}
+func TypetoMap(data interface{}) (map[string]interface{}, error) {
+	result := make(map[string]interface{})
+	err := json.Unmarshal([]byte(TypetoStr(data)), &result)
+	return result, err
+}
+
+// String 转  int
+func StrtoInt(data string) (int, error) {
+	return strconv.Atoi(data)
+}
+
+// String 转  float64
+func StrtoFloat64(data string) (float64, error) {
+	return strconv.ParseFloat(data, 64)
+}
+
+// String 转  bool
+func StrtoBool(data string) (bool, error) {
+	return strconv.ParseBool(data)
+}
+
+// String 转  []byte
+func StrtoBytes(data string) ([]byte, error) {
+	return json.Marshal(data)
+}
+
+// String 转  map[string]interface{}
+func StrtoMap(data string) (map[string]interface{}, error) {
+	result := make(map[string]interface{})
+	err := json.Unmarshal([]byte(data), &result)
+	return result, err
+}
+
+// String 转  []interface{}
+func StrtoSlice(data string) ([]interface{}, error) {
+	result := make([]interface{}, 0)
+	err := json.Unmarshal([]byte(data), &result)
+	return result, err
 }
--- go.mod
+++ go.mod
@@ -21,6 +21,7 @@ require (
 	github.com/richardlehane/mscfb v1.0.4 // indirect
 	github.com/richardlehane/msoleps v1.0.3 // indirect
 	github.com/stretchr/testify v1.8.4 // indirect
+	github.com/tjfoc/gmsm v1.4.1 // indirect
 	github.com/xuri/efp v0.0.0-20230802181842-ad255f2331ca // indirect
 	github.com/xuri/nfp v0.0.0-20230819163627-dc951e3ffe1a // indirect
 	golang.org/x/crypto v0.19.0 // indirect
